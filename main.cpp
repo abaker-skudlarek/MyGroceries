528ed3186c8908403755a838a7f8748cfd2847ca
abaker-skudlarek@mail.csuchico.edu <abaker-skudlarek@mail.csuchico.edu>
2019-02-13 11:46:52 -0800

    finished grocery tasks

--- main.cpp
+++ main.cpp
@@ -1,19 +1,44 @@
 #include<iostream>
 #include<string>
+#include <vector>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
+  vector<string> list; //array of 5 strings
   int numItems = 0;
   char input;
+  string itemName;
 
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
+  while(1)
+  {
+    cout<<"\n==GROCERY LIST MANAGER==";
+    cout<<"\nEnter your choice: ";
+    cout<<"\n (A)dd an item";
+    cout<<"\n (Q)uit";
+    cout<<"\nYour choice (A/Q): ";
+    cin>>input;
 
+    if(input == 'Q')
+      break; 
+    else if(input == 'A')
+    {
+      cout << "Enter name: ";
+      cin >> itemName;
+      list.push_back(itemName);
+      numItems++;
+    }
+  }
+
+  if(numItems == 0)
+  {
+    cout << "No items in list." << endl;
+  }
+
+  for(int i = 0; i < numItems; i++)
+  {
+    cout << i + 1 << ": " << list[i] << endl;
+  }
+ 
   return 0;
 }
